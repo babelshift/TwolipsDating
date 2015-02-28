d1d1ba4c43b118528828057361d4b1feed36eaa5
babelshift <justin.d.skiles@gmail.com>
2015-02-27 23:58:54 -0500

    Initial commit

---
+++ TwolipsDating/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TwolipsDating.ViewModels;
+
+namespace TwolipsDating.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if(User.Identity.IsAuthenticated)
+            {
+                // dashboard
+            }
+            else
+            {
+                // splash
+                HomeViewModel viewModel = new HomeViewModel();
+                return View(String.Empty, "~/Views/Shared/_LayoutSplash.cshtml", viewModel);
+            }
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ TwolipsDating/Utilities/HtmlHelperExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TwolipsDating.Utilities
+{
+    public static class HtmlHelperExtensions
+    {
+        /// <summary>
+        /// Return the Current Version from the AssemblyInfo.cs file.
+        /// </summary>
+        public static string CurrentVersion(this HtmlHelper helper)
+        {
+            try
+            {
+                var version = Assembly.GetExecutingAssembly().GetName().Version;
+                return version.ToString();
+            }
+            catch
+            {
+                return "?.?.?.?";
+            }
+        }
+    }
+}
