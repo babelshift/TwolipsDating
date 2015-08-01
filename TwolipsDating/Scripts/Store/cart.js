479a809fea385a6ec7b51bc2d40266c8a6884f97
babelshift <justin.d.skiles@gmail.com>
2015-08-01 15:49:13 -0400

    Added javascript to update totals and subtotals as the user changes quantities.

---
+++ TwolipsDating/Scripts/Store/cart.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    
+});
+
+function onTextQtyBlur(e, obj, itemNumber) {
+    var itemQuantity = $(obj).val();
+    var itemPrice = $('#Items_' + itemNumber + '__Item_PointsCost').val();
+    var totalItemPrice = itemQuantity * itemPrice;
+    $('#total-cost-' + itemNumber).text(totalItemPrice);
+
+    var subtotal = 0;
+
+    $('.total-cost').each(function () {
+        subtotal += parseInt($(this).text());
+    });
+
+    $('#subtotal').text(subtotal);
+}
