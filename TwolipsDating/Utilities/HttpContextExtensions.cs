56d1a759e35dc74478331bea086b6c4505c7d49f
babelshift <justin.d.skiles@gmail.com>
2015-10-03 15:16:33 -0400

    Only show notification count when > 0. Add ability to turn on/off ads quickly.

---
+++ TwolipsDating/Utilities/HttpContextExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TwolipsDating.Utilities
+{
+    public static class HttpContextExtensions
+    {
+        public static bool AreAdsEnabled(this HttpContext context)
+        {
+            return false;
+        }
+    }
+}
